67292f15b28176c43852e8c808589355f2c45c26
JavelinDD <136085113+JavelinDD@users.noreply.github.com>
2025-02-27 15:50:02 +0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mem(a) memset(a,0,sizeof(a))
+#define jiasu ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define int long long
+#define endl "\n"
+const int maxn=1000005;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod=1e9+7;
+const double pi=acos(-1.0);
+const int INF =0x3f3f3f3f;
+const int inf = 0x3f3f3f3f;
+signed main(){
+	jiasu;
+	cout<<"hello";	
+} 
